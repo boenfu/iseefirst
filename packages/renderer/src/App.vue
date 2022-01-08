533f6f188e61ecfe4d313b1118e7e3e4aad512ef
boenfu <1997@boenfu.cn>
2022-01-08 20:34:50 +0800

    add deps

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,29 +1,17 @@
 <script lang="ts" setup>
-import Enter from './components/Enter.vue';
+// import { reactive, onMounted, defineComponent } from 'vue'
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="../assets/logo.svg"
-    width="150"
-  >
-  <n-button>????</n-button>
-  <enter />
+  <div>content</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 60px auto;
-  max-width: 700px;
-}
-fieldset {
-  margin: 2rem;
-  padding: 1rem;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
